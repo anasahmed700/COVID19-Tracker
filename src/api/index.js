15defed4974e256d1a1c253d78f1f7ba8e6d917f
Anas Ahmed <anasahmed700@gmail.com>
2020-07-02 16:59:33 +0500

    created cards component

---
+++ src/api/index.js
@@ -0,0 +1,16 @@
+import axios from  'axios'
+
+const apiUrl = 'https://covid19.mathdro.id/api'
+
+export const fetchData = async () => {
+    try {
+        // destructuring desired data from API
+        const { data: {confirmed, recovered, deaths, lastUpdate} } = await axios.get(apiUrl)
+        
+        return {confirmed, recovered, deaths, lastUpdate,}
+        
+    } catch (error) {
+        console.log(error)    
+    }
+}
+
